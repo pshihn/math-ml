eb877ecaf8ada9a93beaa0e116c560af2f603a64
Preet Shihn <shihn@Preets-MacBook-Pro.local>
2018-10-29 15:33:33 -0700

    sqrt alignment

--- src/msqrt.ts
+++ src/msqrt.ts
@@ -9,9 +9,10 @@ export class MathSqrtElement extends MathMLElement {
       ${HorizFlex}
       :host {
         display: inline-block;
+        vertical-align: bottom;
       }
       .msqrtContent {
-        padding: 0 0.2em;
+        padding: 1px 0.05em 0 0.2em;
         border-top: solid thin;
       }
       #msqrtGlyphSpan {
