13a4960c79be775a1674c6309217efd62d8b1e00
Preet Shihn <shihn@Preets-MacBook-Pro.local>
2018-10-30 23:52:42 -0700

    msup enhancements

--- dist/msup.d.ts
+++ dist/msup.d.ts
@@ -1,4 +1,5 @@
 import { MathMLElement, TemplateResult } from './mathml-element.js';
 export declare class MathSupElement extends MathMLElement {
     render(): TemplateResult;
+    refreshSlot(): void;
 }
