f3f5353a27288abab5f99e0d26957f892c0de9b2
Preet Shihn <shihn@Preets-MacBook-Pro.local>
2018-10-31 16:55:00 -0700

    multiscripts

--- dist/mmultiscripts.d.ts
+++ dist/mmultiscripts.d.ts
@@ -1,7 +1,5 @@
 import { MathMLElement, TemplateResult } from './mathml-element.js';
 export declare class MathMultiScriptsElement extends MathMLElement {
     render(): TemplateResult;
-    updated(): void;
-    private clearNodes;
     private refreshSlot;
 }
